fada3f7ef8da4026eb2b1ee39af380e453925e77
AliciaMerlino <acmerlino1@gmail.com>
2020-01-09 15:54:15 +0000

    use constant to show default hit points

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,13 @@
 
 class Player
 
+  HIT_POINTS = 100
+
   attr_reader :name, :hit_points
 
   def initialize(name)
     @name = name
-    @hit_points = 100
+    @hit_points = HIT_POINTS
   end 
 
   def attack
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,18 @@
 require 'player'
 
-describe 'Player' do 
-  it 'should return players name' do 
-    subject = Player.new("Ash")
-    expect(subject.name).to eq "Ash"
+describe 'Player' do
+  subject(:ash) { Player.new("Ash") }
+  subject(:brock) { Player.new("Brock") }
+
+  describe '#name' do
+    it 'should return players name' do
+      expect(ash.name).to eq "Ash"
+    end
   end
-end 
+
+  describe '#hit_points' do
+    it 'returns the HP' do
+      expect(brock.hit_points).to eq Player::HIT_POINTS
+    end
+  end
+end
