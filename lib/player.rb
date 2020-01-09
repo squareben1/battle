5bce46243c9c44a7654ccad8772640e103b165e3
AliciaMerlino <acmerlino1@gmail.com>
2020-01-09 15:44:11 +0000

    if player is attacked, they lose points

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,15 @@
 
 class Player
 
-  attr_reader :name 
+  attr_reader :name, :hit_points
 
   def initialize(name)
     @name = name
+    @hit_points = 100
   end 
 
-  # def name
-  #   @name
-  # end 
+  def attack
+    @hit_points -= 10
+  end
 
 end 
